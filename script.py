42171152d3afedb0fc319562f2e88ed742bf2ca5
caldayham <caldayham@gmail.com>
2022-12-15 15:58:17 -0800

    stuff

--- script.py
+++ script.py
@@ -13,14 +13,14 @@ print(' ')
 print('==============================================================================')
 print('==============================================================================')
 
-
 # define the template for the 'game' object (customized data class)
 class C4Model:
 
-    def __init__(game):
-        game.p1 = 'player 1'
-        game.p2 = 'player 2'
-        game.rows = [
+    # the initial constructor for the c4 game object
+    def __init__(self, p1, p2,):
+        self.p1 = p1
+        self.p2 = p2
+        self.gameBoard = [
             ['','','','','','',''],
             ['','','','','','',''],
             ['','','','','','',''],
@@ -29,22 +29,24 @@ class C4Model:
             ['','','','','','',''],
             ['','','','','','','']
         ]
+        self.isComplete = False
 
-    # function that takes the boad array of arrays and prints the board as a string that is readable in the console
-    def __str__():
-        print('here are the rows lol')
+    # function that takes the board array of arrays and prints the board as a string that is readable in the console
+    def printBoard(self):
+        return 'hello'
 
     # function that returns weather or not the game is still in play
-    def __bool__():
+    def __bool__(self):
         return False
 
     # function that allows one of the players to drop their tile by specifying a number 1-7 for the column
-    def drop(player):
+    def drop(self):
         print('cool')
     
     # function that returns the board as an array of 6 items representing the rows, with each item being its own array of 7 items representing individual slots
-    def board():
-        print('cool')
+    def board(self):
+        print('this is board')
+        return self.gameBoard
 
     # function that returns a hash table (dict) with two items mapping the player numbers (1 & 2) to their respective names / colors
     def player_names():
@@ -63,3 +65,20 @@ class C4Model:
     # function that returns the winner of the game, or 'none' if the game has not ended or has ended in a draw
     def winner():
         return None
+
+
+
+#============================================================
+#============================================================
+
+# initialize a new game 'g' based on the connect 4 model above
+g = C4Model(p1='red', p2='orange')
+
+remainingSpaces = 6*7
+while (remainingSpaces >0):
+    g.printBoard
+    input('')
+else:
+    print('there are no remaining spaces!')
+    exit
+
